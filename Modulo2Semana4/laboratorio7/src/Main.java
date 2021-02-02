af71c92aa9b8591e53d74363b76f774daf0698d7
leorumma <leonardo.ruma@gmail.com>
2021-02-02 03:11:24 -0300

    Update Main.java

--- Modulo2Semana4/laboratorio7/src/Main.java
+++ Modulo2Semana4/laboratorio7/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         MyBuffer myBuffer = new MyBuffer(10);
-        Produtor[] produtors = new Produtor[4];       // Threads leitores
-        Consumidor[] consumidors = new Consumidor[3];   // Threads escritores
+        Produtor[] produtors = new Produtor[4];      
+        Consumidor[] consumidors = new Consumidor[3];  
 
         for (int i = 0; i < produtors.length; i++) {
             produtors[i] = new Produtor(i + 1, (i + 1) * 500, myBuffer);
